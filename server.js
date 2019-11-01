7a980ce95bc13026e564a3131181dfb50fc395a3
islom247 <islomiddin.sodiqov@gmail.com>
2019-11-01 20:31:46 +0300

    removed unnecessary commented lines;

--- server.js
+++ server.js
@@ -24,10 +24,8 @@ app.get('/api/service1', (req, res) => {
             [{no: ''}, {no: ''}, {no: ''}, {no: ''}, {no: ''}],
             [{no: ''}, {no: ''}, {no: ''}, {no: ''}, {no: ''}],
         ];
-
-        for(let i = 0; i < 25; i++){
+        for (let i = 0; i < 25; i++) {
             let cell;
-
             if( i === 24) {
                 cell = body.substring(
                     body.indexOf("id=\"cell-id-" + i + "\"") + 1,
@@ -39,95 +37,55 @@ app.get('/api/service1', (req, res) => {
                     body.indexOf("id=\"cell-id-" + (i + 1) + "\"")
                 );
             }
-
             table[ Math.floor(i / 5) ][i % 5].index = i;
-
-            if( cell.includes( "class=\"Cell-block--1oNaD\"")){
+            if (cell.includes( "class=\"Cell-block--1oNaD\"")) {
                 table[ Math.floor(i / 5) ][i % 5].no = '*';
                 table[ Math.floor(i / 5) ][i % 5].color = 'black';
             } else {
-
                 const c = cell.charAt( cell.indexOf('</text>') - 1);
-
                 if (c >= '0' && c <= '9') {
                     table[ Math.floor(i / 5) ][i % 5].no = c;
                     table[ Math.floor(i / 5) ][i % 5].color = 'white';
                 }
             }
         }
-
         const clues = {
             across: {},
             down: {}
         };
-
         const acrossClues = body.substring(
             body.indexOf(">Across</h3>") + 1,
             body.indexOf(">Down</h3>")
         );
-
-        // </span></li>
         let i = 0;
-        while ( acrossClues.indexOf("<span class=\"Clue-label--2IdMY\">", i) !== -1){
-
+        while ( acrossClues.indexOf("<span class=\"Clue-label--2IdMY\">", i) !== -1) {
             const no = acrossClues.charAt(acrossClues.indexOf("<span class=\"Clue-label--2IdMY\">", i) + 32);
-
             const clue = acrossClues.substring(
                 acrossClues.indexOf("<span class=\"Clue-text--3lZl7\">", i) + 31,
                 acrossClues.indexOf("</span></li>", i)
             );
-
             i++;
-
             clues.across[no] = clue;
         }
-
         const downClues = body.substring(
             body.indexOf(">Down</h3>") + 1,
             body.indexOf(">Sitemap</a>")
         );
-
         i = 0;
-
-        while ( downClues.indexOf("<span class=\"Clue-label--2IdMY\">", i) !== -1){
-
+        while (downClues.indexOf("<span class=\"Clue-label--2IdMY\">", i) !== -1){
             const no = downClues.charAt(downClues.indexOf("<span class=\"Clue-label--2IdMY\">", i) + 32);
-
             const clue = downClues.substring(
                 downClues.indexOf("<span class=\"Clue-text--3lZl7\">", i) + 31,
                 downClues.indexOf("</span></li>", i)
             );
-
             i++;
-
             clues.down[no] = clue;
         }
-
         res.send({
             table: table,
             clues: clues
         });
-
     });
-
-
-
-    // res.send(
-    //     {
-    //         table: [
-    //             ['*', '1', '2', '3', '4']
-    //         ],
-    //         clues: {
-    //             across: {
-    //                 1: "clue1",
-    //                 3: "clue2"
-    //             },
-    //             down: {
-    //                 2: "clue1",
-    //                 3: "clue2"
-    //             }
-    //         }
-    //     });
 });
 
 app.get('/api/service2', (req, res) => {
@@ -150,11 +108,9 @@ app.get('/api/service2', (req, res) => {
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
     app.use(express.static(path.join(__dirname, 'client/build')));
-
     // Handle React routing, return all requests to React app
     app.get('*', function(req, res) {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
 }
-
 app.listen(port, () => console.log(`listening on port ${port}`));
