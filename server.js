6e33909b820c5e694b30f428b6e2b777b1203425
islom247 <islomiddin.sodiqov@gmail.com>
2019-11-01 23:43:12 +0300

    a few links scraped

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 // API calls
 app.get('/api/service1', (req, res) => {
@@ -15,7 +15,7 @@ app.get('/api/service1', (req, res) => {
     // get html code of website
     request({
         uri: "https://www.nytimes.com/crosswords/game/mini",
-    }, function(error, response, body) {
+    }, function (error, response, body) {
 
         let table = [
             [{no: ''}, {no: ''}, {no: ''}, {no: ''}, {no: ''}],
@@ -26,7 +26,7 @@ app.get('/api/service1', (req, res) => {
         ];
         for (let i = 0; i < 25; i++) {
             let cell;
-            if( i === 24) {
+            if (i === 24) {
                 cell = body.substring(
                     body.indexOf("id=\"cell-id-" + i + "\"") + 1,
                     body.indexOf("</g><g data-group=\"grid\">")
@@ -37,15 +37,15 @@ app.get('/api/service1', (req, res) => {
                     body.indexOf("id=\"cell-id-" + (i + 1) + "\"")
                 );
             }
-            table[ Math.floor(i / 5) ][i % 5].index = i;
-            if (cell.includes( "class=\"Cell-block--1oNaD\"")) {
-                table[ Math.floor(i / 5) ][i % 5].no = '*';
-                table[ Math.floor(i / 5) ][i % 5].color = 'black';
+            table[Math.floor(i / 5)][i % 5].index = i;
+            if (cell.includes("class=\"Cell-block--1oNaD\"")) {
+                table[Math.floor(i / 5)][i % 5].no = '*';
+                table[Math.floor(i / 5)][i % 5].color = 'black';
             } else {
-                const c = cell.charAt( cell.indexOf('</text>') - 1);
+                const c = cell.charAt(cell.indexOf('</text>') - 1);
                 if (c >= '0' && c <= '9') {
-                    table[ Math.floor(i / 5) ][i % 5].no = c;
-                    table[ Math.floor(i / 5) ][i % 5].color = 'white';
+                    table[Math.floor(i / 5)][i % 5].no = c;
+                    table[Math.floor(i / 5)][i % 5].color = 'white';
                 }
             }
         }
@@ -58,7 +58,7 @@ app.get('/api/service1', (req, res) => {
             body.indexOf(">Down</h3>")
         );
         let i = 0;
-        while ( acrossClues.indexOf("<span class=\"Clue-label--2IdMY\">", i) !== -1) {
+        while (acrossClues.indexOf("<span class=\"Clue-label--2IdMY\">", i) !== -1) {
             const no = acrossClues.charAt(acrossClues.indexOf("<span class=\"Clue-label--2IdMY\">", i) + 32);
             const clue = acrossClues.substring(
                 acrossClues.indexOf("<span class=\"Clue-text--3lZl7\">", i) + 31,
@@ -72,7 +72,7 @@ app.get('/api/service1', (req, res) => {
             body.indexOf(">Sitemap</a>")
         );
         i = 0;
-        while (downClues.indexOf("<span class=\"Clue-label--2IdMY\">", i) !== -1){
+        while (downClues.indexOf("<span class=\"Clue-label--2IdMY\">", i) !== -1) {
             const no = downClues.charAt(downClues.indexOf("<span class=\"Clue-label--2IdMY\">", i) + 32);
             const clue = downClues.substring(
                 downClues.indexOf("<span class=\"Clue-text--3lZl7\">", i) + 31,
@@ -89,27 +89,43 @@ app.get('/api/service1', (req, res) => {
 });
 
 app.get('/api/service2', (req, res) => {
-    res.send(
-        {
-            answers: [
-                {
-                    clue: "peace of cake",
-                    ans: "easy"
-                },
-                {
-                    clue: "the best guy from Tajikistan",
-                    ans: "islom"
-                },
-            ]
-
-        });
+    request({uri: "https://nytimescrosswordanswers.com/new-york-times-the-mini-crossword-answers"},
+        (error, response, body) => {
+            let start_ind = body.indexOf("<h1><a href=\"") + 14;
+            let end_ind = body.indexOf(">", start_ind) - 1;
+            let today_uri = "https://nytimescrosswordanswers.com/" + body.substring(start_ind, end_ind);
+            request({uri: today_uri},
+                (error, response, body) => {
+                    let start_ind = body.indexOf("<div class=\"card results-card\">");
+                    let end_index = body.indexOf("</div><!-- results-card -->");
+                    let clues = body.substring(start_ind, end_index);
+                    let answers = [];
+                    let tmp_ind = start_ind;
+                    while (clues.indexOf("<h1>" != -1)) {
+                        let start = body.indexOf("<h1><a href") + 14;
+                        let end = body.indexOf(">", start) - 1;
+                        let ans_uri = "https://nytimescrosswordanswers.com/" + body.substring(start, end);
+                        request({uri: ans_uri},
+                            (error, response, body) => {
+                                let start_ind = body.indexOf("clue", body.indexOf("<p><strong>")) + 6;
+                                let end_ind = body.indexOf("</a>", start_ind);
+                                let clue = body.substring(start_ind, end_ind);
+                                res.send({ans_uri});
+                            }
+                        );
+                    }
+                }
+            );
+        }
+    );
+    //res.send({answers: [{}, {},]});
 });
 
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
     app.use(express.static(path.join(__dirname, 'client/build')));
     // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
+    app.get('*', function (req, res) {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
 }
